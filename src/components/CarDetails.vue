a0973cdeaa5443bd270675a48baf64666c1a04b3
Alexey Dobrynin <ag.dobrynin@gmail.com>
2020-01-18 18:43:01 +0400

    Car details component

---
+++ src/components/CarDetails.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+    div.card.mt-3
+        h5.card-header Car details
+        div.card-body
+            h5.card-title Model
+            p.card-text
+                ul
+                    li Model: AAAAA
+                    li Mark: BBBB
+</template>
+
+<script>
+    export default {
+        name: "CarDetails"
+    }
+</script>
+
+<style scoped>
+
+</style>
