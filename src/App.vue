127b13a9e34530e2174cd92e087fe00897212b7c
Alexey Dobrynin <ag.dobrynin@gmail.com>
2020-01-18 15:55:51 +0400

    Show and hide navigation when page is compact

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,55 @@
 <template lang="pug">
-div
-    div.appMian
-        nav.navbar.navbar-expand-lg.navbar-light.bg-light
-            a.navbar-brand(href="/")
-                img(alt="Vue logo" src="@/assets/logo.png" width="30" height="30")
-            button.navbar-toggler(
-                type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation")
+    div
+        div.appMian
+            nav.navbar.navbar-expand-lg.navbar-light.bg-light
+                <router-link to="/"  @click.native="show = false">
+                    img(alt="Vue logo" src="@/assets/logo.png" width="30" height="30")
+                </router-link>
+                button.navbar-toggler(
+                    type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                    @click.stop="toggleNavbar"
+                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation")
                     span.navbar-toggler-icon &nbsp;
-            .collapse.navbar-collapse#navbarNavAltMarkup
-                .navbar-nav
-                    <router-link to="/cars" class="nav-item nav-link" exact-active-class="active">Cars</router-link>
-                    <router-link to="/about" class="nav-item nav-link" exact-active-class="active">About</router-link>
-        .container <router-view/>
-    footer.page-footer.font-small
-        .footer-copyright.text-center.py-3 © 2020 Copyright:
-            | #[a(href="https://www.udemy.com/") Lesson with UDEMY.COM ]
+                .collapse.navbar-collapse#navbarNavAltMarkup(:class="{'show': show }")
+                    .navbar-nav
+                        <router-link to="/cars"
+                            | class="nav-item nav-link" exact-active-class="active"
+                            | @click.native="show = false">Cars</router-link>
+                        <router-link to="/about"
+                            | class="nav-item nav-link" exact-active-class="active"
+                            |  @click.native="show = false">About</router-link>
+            .container <router-view/>
+        footer.page-footer.font-small
+            .footer-copyright.text-center.py-3 © 2020 Copyright:
+                | #[a(href="https://www.udemy.com/") Lesson with UDEMY.COM ]
 </template>
 
+<script>
+    export default {
+        name: 'app',
+        data() {
+            return {
+                show: false,
+            }
+        },
+        methods: {
+            toggleNavbar() {
+                this.show = !this.show;
+            }
+        },
+    }
+</script>
+
 <style>
-    .appMain {margin-bottom: 60px;}
-    a.active { color: red!important;}
-    footer {background: #eee;}
+    .appMain {
+        margin-bottom: 60px;
+    }
+
+    a.active {
+        color: red !important;
+    }
+
+    footer {
+        background: #eee;
+    }
 </style>
